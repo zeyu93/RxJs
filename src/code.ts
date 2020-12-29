743bd96c2622cb62569d901c1f67568d5dd45e56
Zeyu Liu <thepho4life@gmail.com>
2020-12-29 13:29:27 -0500

    adding to index html

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,20 @@
 import { Observable } from "rxjs";
 
 const observable = new Observable((observer: any) => {
-  observer.next("hey -> This is from the next() function");
-  observer.next("hey 2 -> This is also from the next() function");
-  observer.complete("completed -> This is from the complete() function");
-  observer.next("hey -> This is from the next() function");
+  try {
+    observer.next("hey -> This is from the next() function");
+    observer.next("hey 2 -> This is also from the next() function");
+    setInterval(() => {
+      observer.next(
+        "emitting value every two seconds, observer is subscribed to handle this value"
+      );
+    }, 2000);
+  } catch (e) {
+    observer.error(e);
+  }
 });
 
-observable.subscribe(
+const observer = observable.subscribe(
   (value: any) => {
     addItem(value);
   },
@@ -19,6 +26,7 @@ observable.subscribe(
   }
 );
 
+
 function addItem(value: any) {
   const list = document.getElementById("output");
   const node = document.createElement("li");
@@ -26,3 +34,9 @@ function addItem(value: any) {
   node.appendChild(textNode);
   list.appendChild(node);
 }
+
+let cancelButton = document.getElementById("cancel");
+
+cancelButton.addEventListener("click", () => {
+    observer.unsubscribe()
+});
